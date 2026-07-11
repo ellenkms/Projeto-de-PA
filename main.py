b1944509cf98f1c26a36f8e65dd85cceb8f2831e
Ricardo Silva Machado <ricardomachado1003@gmail.com>
2026-07-11 07:39:18 -0300

    Renomeia arquivo para main.py e adiciona retangulos

--- main.py
+++ main.py
@@ -4,51 +4,59 @@ from tkinter import ttk
 # Quando mouse é pressionado
 def iniciar_figura_nova(event): 
     global figura_nova
-    if tipo_figura_var.get() == 'Linha':
-        figura_nova = ("linha", (event.x, event.y, event.x, event.y))
-    else :
-        figura_nova = ("rabisco", [(event.x, event.y)])
+    tipo = tipo_figura_var.get()
+    
+    if tipo == 'Rabisco':
+        figura_nova = (tipo, [(event.x, event.y)])
+    else: 
+        figura_nova = (tipo, (event.x, event.y, event.x, event.y))
 
 # Quando mouse é movido com o botão pressionado
 def atualizar_figura_nova(event):
     global figura_nova
-    if figura_nova[0] == "rabisco":
-        figura_nova[1].append((event.x, event.y))
-    else : # figura_nova[0] == "linha"
-        figura_nova = ("linha", (figura_nova[1][0], figura_nova[1][1], event.x, event.y))
+    tipo, valores = figura_nova
+    
+    if tipo == "Rabisco":
+        valores.append((event.x, event.y))
+        figura_nova = (tipo, valores)
+    else: 
+        figura_nova = (tipo, (valores[0], valores[1], event.x, event.y))
+        
     desenhar_figuras()
     desenhar_figura_nova()
 
 # Quando mouse é solto
 def incluir_figura_nova(event): 
-    if not incompleta(figura_nova): # para evitar incluir figuras incompletas, como uma linha sem comprimento ou um rabisco com um único ponto
+    if not incompleta(figura_nova): 
         figuras.append(figura_nova) 
     desenhar_figuras()
 
 def desenhar_figuras():
     canvas.delete("all")
     for fig, values in figuras:
-        if fig == "linha":
+        if fig == "Linha":
             canvas.create_line(values[0], values[1], values[2], values[3])
-        else : # fig == "rabisco"
+        elif fig == "Retângulo":
+            canvas.create_rectangle(values[0], values[1], values[2], values[3])
+        else: # fig == "Rabisco"
             canvas.create_line(values)
 
 def desenhar_figura_nova():
-    fig, values = figura_nova
-    if fig == "linha":
-        canvas.create_line(values[0], values[1], values[2], values[3], dash=(4, 2))
-    else : # fig == "rabisco"
-        canvas.create_line(values, dash=(4, 2))
+    if figura_nova:
+        fig, values = figura_nova
+        if fig == "Linha":
+            canvas.create_line(values[0], values[1], values[2], values[3], dash=(4, 2))
+        elif fig == "Retângulo":
+            canvas.create_rectangle(values[0], values[1], values[2], values[3], dash=(4, 2))
+        else: # fig == "Rabisco"
+            canvas.create_line(values, dash=(4, 2))
 
 def incompleta(figura):
     fig, values = figura
-    if fig == "linha":
-        return (values[0], values[1]) == (values[2], values[3])
-    else : # fig == "rabisco"
+    if fig == "Rabisco":
         return len(values) <= 1
-
-
-
+    else: 
+        return (values[0], values[1]) == (values[2], values[3])
 
 #******* MAIN *******#
 
@@ -58,26 +66,21 @@ figura_nova = None # Figura que está sendo desenhada, mas ainda não foi inclu�
 root = Tk()
 frame = Frame(root)
 
-# Widgets arranjados com Layout grid dentro de frame
 paddings = {'padx': 5, 'pady': 5} 
 
-# label
-label = ttk.Label(frame,  text='Linha ou Rabisco:')
+label = ttk.Label(frame,  text='Figura:')
 label.grid(column=0, row=0, sticky=W, **paddings)
 
-# option menu
-tipo_figura_var = StringVar(root) # Guarda o tipo de figura selecionado no option menu (linha ou rabisco)
+tipo_figura_var = StringVar(root) 
 option_menu = ttk.OptionMenu(frame, tipo_figura_var,
-                             'Linha', 'Linha', 'Rabisco')
+                             'Linha', 'Linha', 'Rabisco', 'Retângulo')
 option_menu.grid(column=1, row=0, sticky=W, **paddings)
 
-# Área de desenho
 canvas = Canvas(frame, bg='white', width=600, height=600)
 canvas.grid(column=0, row=1, columnspan=2, sticky=W, **paddings)
 
 frame.pack()
 
-# Eventos de mouse associados ao canvas - com seus callbacks
 canvas.bind('<ButtonPress-1>', iniciar_figura_nova)
 canvas.bind('<B1-Motion>', atualizar_figura_nova)
 canvas.bind('<ButtonRelease-1>', incluir_figura_nova)
